e7b92659afc94215d5d4c10cd4c068d66d1df29f
Marta Małachowicz <68849042+malachowiczmarta@users.noreply.github.com>
2021-01-14 15:05:23 +0100

    create rootReducer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 import "./styles.css";
 import Users from "./users/containers/Users";
 import Home from "./home/containers/Home";
+import rootReducer from "./rootReducer";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default function App() {
   return (
     <div className="App">
-      <Router>
-        <div className="container">
-          <Home />
-          <Users />
-        </div>
-        <Switch>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
+      <Provider store={store}>
+        <Router>
+          <div className="container">
             <Home />
-          </Route>
-        </Switch>
-      </Router>
+            <Users />
+          </div>
+          <Switch>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </Provider>
     </div>
   );
 }
--- src/home/containers/Home.js
+++ src/home/containers/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 import HomeButtons from "../components/HomeButtons";
+import { connect } from "react-redux";
+import { fetchPosts } from "../redux";
 
 export default function Home() {
+  function fatchData() {}
+
   return (
     <div className="home-container">
       <h1>HOME</h1>
